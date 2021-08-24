2b4c992d5f5d3bbbbe92d7a7628b9cc97a355794
Renan Valladão <renancvalladao@gmail.com>
2021-08-23 21:06:46 -0300

    Fundamentos

---
+++ exercicios/src/fundamentos/Ternario.java
@@ -0,0 +1,20 @@
+package fundamentos;
+
+public class Ternario {
+
+	public static void main(String[] args) {
+		
+		double media = 8.6;
+		String resultadoFinal = media >= 7.0 ? "aprovado." : "em recuperação.";
+		
+		System.out.println("O aluno está " + resultadoFinal);
+		
+		double nota = 7.3;
+		boolean bomComportamento = true;
+		boolean passouPorMedia = nota >= 7;
+		boolean temDesconto = bomComportamento && passouPorMedia;
+		String resultado = temDesconto ? "Sim." : "Não.";
+		
+		System.out.println("Tem desconto? " + resultado);
+	}
+}
