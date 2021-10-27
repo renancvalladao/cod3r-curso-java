6f5b782b3b3f430eb59201bbc989bb0b307f1ce8
Renan Valladão <renancvalladao@gmail.com>
2021-10-26 21:02:42 -0300

    ClassesMétodos

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -6,11 +6,25 @@ public class Data {
 	int mes;
 	int ano;
 	
+	Data() {
+//		dia = 1;
+//		mes = 1;
+//		ano = 1970;
+		this(1, 1, 1970);
+	}
+	
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+	
 	String obterDataFormatada() {
-		return String.format("%d/%d/%d", dia, mes, ano);
+		final String formato = "%d/%d/%d";
+		return String.format(formato, dia, mes, ano);
 	}
 	
 	void imprimirDataFormatada() {
-		System.out.println(obterDataFormatada());
+		System.out.println(this.obterDataFormatada());
 	}
 }
--- exercicios/src/classe/Produto.java
+++ exercicios/src/classe/Produto.java
@@ -4,7 +4,16 @@ public class Produto {
 
 	String nome;
 	double preco;
-	double desconto;
+	static double desconto = 0.25;
+	
+	Produto() {
+		
+	}
+	
+	Produto(String nomeInicial, double precoInicial) {
+		nome = nomeInicial;
+		preco = precoInicial;
+	}
 	
 	double precoComDesconto() {
 		return preco * (1 - desconto);
