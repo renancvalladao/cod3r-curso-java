fbc291003bb5800d75060c88150994e7dc7f2d97
Renan Valladão <renancvalladao@gmail.com>
2022-02-27 16:42:47 -0300

    Orientação a objetos

---
+++ exercicios/src/oo/heranca/Jogo.java
@@ -0,0 +1,29 @@
+package oo.heranca;
+
+public class Jogo {
+	
+	public static void main(String[] args) {
+		
+		Monstro monstro = new Monstro();
+		monstro.x = 10;
+		monstro.y = 10;
+		
+		Heroi heroi = new Heroi(10, 11);
+		
+		System.out.println("Monstro tem => " + monstro.vida);
+		System.out.println("Heroi tem => " + heroi.vida);
+		
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		
+		monstro.andar(Direcao.NORTE);
+		heroi.atacar(monstro);
+		monstro.atacar(heroi);
+		
+		System.out.println("Monstro tem => " + monstro.vida);
+		System.out.println("Heroi tem => " + heroi.vida);
+	}
+}
